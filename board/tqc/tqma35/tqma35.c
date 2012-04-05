f7c8197833cf98b3119d4a04b8d3885f5578d4b7
Markus niebel <Markus.Niebel@tqs.de>
2012-04-05 14:42:20 +0200

    TQMa35: fix board_init_eth
    The board_init_eth causes a double invocation of cpu_init_eth if compiling without
    external SMC9118. Fix it and return result of cpu_init_eth
    
    Signed-off-by: Markus niebel <Markus.Niebel@tqs.de>
    Signed-off-by: Stephan Linz <stephan.linz@clondiag.com>

--- board/tqc/tqma35/tqma35.c
+++ board/tqc/tqma35/tqma35.c
@@ -29,6 +29,7 @@
 #include <asm/arch/mx35_pins.h>
 #include <asm/arch/iomux.h>
 #include <i2c.h>
+#include <netdev.h>
 
 #ifdef CONFIG_ARCH_MMU
 #include <asm/mmu.h>
@@ -546,8 +547,7 @@ extern int smc911x_initialize(u8 dev_num, int base_addr);
 int board_eth_init(bd_t *bis)
 {
 	int rc = -ENODEV;
-
-	cpu_eth_init(bis);
+	rc = cpu_eth_init(bis);
 
 #if defined(CONFIG_SMC911X)
 	rc = smc911x_initialize(0, CONFIG_SMC911X_BASE);
